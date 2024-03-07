21572af61acaf4f01c4546e3a4c723e91b44bdb5
Emanuel Kuhn <kuhn.emanuel@gmail.com>
2024-03-07 09:15:56 +0100

    Update preprocessing

--- preprocessing/preprocess_floorplan.py
+++ preprocessing/preprocess_floorplan.py
@@ -20,23 +20,59 @@ class PreprocessingFloorplan(RplanWrapper):
             return None
     
     @staticmethod
-    def render_palette_image(fp):
+    def render_rgb_image(fp: FloorPlan):
         if fp is None:
             return None
         
         try:
-            img = render_room_number_palette_image(fp, 5, True)
+            img = render_room_number_palette_image(fp, 4, True)
+
+            img = img.convert("RGB")
+        
+            return datasets.Image(decode=False).encode_example(img)
+        except:
+            return None
+
+    @staticmethod
+    def render_room_number_image(fp: FloorPlan):
+        if fp is None:
+            return None
+        
+        try:
+            img = render_room_number_palette_image(fp, 4, True)
         
             return datasets.Image(decode=False).encode_example(img)
         except:
             return None
 
 
-    def make_floorplan_dict(self, ids):
+    def make_floorplan_dict_rgb(self, ids):
 
         floorplans: List[FloorPlan] = process_map(self.load_floorplan, ids, desc="Parsing floor plans", chunksize=10)
 
-        imgs = process_map(self.render_palette_image, floorplans, desc="drawing images", chunksize=10)
+        imgs = process_map(self.render_rgb_image, floorplans, desc="drawing images", chunksize=10)
+        
+        num_rooms = [len(fp.room_categories) if fp is not None else None for fp in floorplans]
+
+        df = pd.DataFrame({
+            "id": ids,
+            "img": imgs,
+            "num_rooms": num_rooms
+        })
+
+        df = df.dropna()
+
+        result_dict = df.to_dict(orient="list")
+
+        return result_dict
+
+
+
+    def make_floorplan_dict_room_number(self, ids):
+
+        floorplans: List[FloorPlan] = process_map(self.load_floorplan, ids, desc="Parsing floor plans", chunksize=10)
+
+        imgs = process_map(self.render_room_number_image, floorplans, desc="drawing images", chunksize=10)
         
         num_rooms = [len(fp.room_categories) if fp is not None else None for fp in floorplans]
 
@@ -56,6 +92,5 @@ class PreprocessingFloorplan(RplanWrapper):
         del result_dict["fp"]
 
         return result_dict
-
 
 
--- run_preprocessing.py
+++ run_preprocessing.py
@@ -22,7 +22,11 @@ import datasets
 from preprocessing.preprocess_floorplan import PreprocessingFloorplan
 
 class PreprocessingStyle(Enum):
-    CONSISTENT_WALL_THICKNESS = "ds_rplan_processed_geometry_rgb"
+    CONSISTENT_WALL_THICKNESS_RGB = "ds_rplan_processed_geometry_rgb"
+
+    # Stored as categorical value per pixel
+    # CONSISTENT_WALL_THICKNESS_FOR_MASKING = "ds_rplan_processed_geometry_masking"
+    
     CATEGORY_CHANNEL = "ds_rplan_category"
     RPLANPY = "ds_rplanpy_rgb"
 
@@ -50,36 +54,33 @@ def main():
         print(field)
         getattr(conf, field.name)
 
-    ids = list(range(10000))
+    ids = list(range(1000))
 
     split = datasets.NamedSplit("debugging")
 
     if conf.method == PreprocessingStyle.CATEGORY_CHANNEL:
         preprocessing = PreprocessingCategory(conf.rplan_dataset_path)
 
-        category_dict = preprocessing.make_category_dict(ids)
+        data_dict = preprocessing.make_category_dict(ids)
 
-        ds = datasets.arrow_dataset.Dataset.from_dict(category_dict, split=split)
     elif conf.method == PreprocessingStyle.RPLANPY:
         preprocessing = PreprocessingRplanpy(conf.rplan_dataset_path)
 
-        rplanpy_dict = preprocessing.make_rplanpy_dict(ids)
+        data_dict = preprocessing.make_rplanpy_dict(ids)
 
-        ds = datasets.arrow_dataset.Dataset.from_dict(rplanpy_dict, split=split)
-    elif conf.method == PreprocessingStyle.CONSISTENT_WALL_THICKNESS:
+    elif conf.method == PreprocessingStyle.CONSISTENT_WALL_THICKNESS_RGB:
         preprocessing = PreprocessingFloorplan(conf.rplan_dataset_path)
 
-        fp_dict = preprocessing.make_floorplan_dict(ids)
-
-        ds = datasets.arrow_dataset.Dataset.from_dict(fp_dict, split=split)
-
+        data_dict = preprocessing.make_floorplan_dict_rgb(ids)
         
     else:
         raise ValueError(f"Unknown preprocessing method: {conf.method}")
 
+    ds = datasets.arrow_dataset.Dataset.from_dict(data_dict, split=split, info=datasets.DatasetInfo(description=conf.method.value))
+
     os.makedirs(conf.save_datasets_path, exist_ok=True)
 
-    ds.save_to_disk(Path(conf.save_datasets_path) / conf.method.value)
+    ds.save_to_disk(Path(conf.save_datasets_path) / conf.method.value / split._name)
 
 
 

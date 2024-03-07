5548cbf3c989fb7c1f0df0849466673f1f45fa2c
Emanuel Kuhn <kuhn.emanuel@gmail.com>
2024-03-07 14:46:04 +0100

    Factor out save_features

--- predict_embeddings.py
+++ predict_embeddings.py
@@ -62,30 +62,21 @@ def model_huggingface_url_to_preprocessing_style(huggingface_url) -> Preprocessi
     else:
         raise ValueError(f"Unmapped {huggingface_url=}")
 
-def main():
-
-    conf = OmegaConf.structured(Config)
-    conf.merge_with_cli()
-    conf = typing.cast(Config, conf)
-
-
-    for field in dataclasses.fields(Config):
-        getattr(conf, field.name)
-
+def save_features(wandb_prefix, wandb_model_ref, split, save_datasets_path, features_cache_folder):
     # Load model
-    model_context = WandbModelContext(conf.wandb_prefix, conf.wandb_model_ref)
+    model_context = WandbModelContext(wandb_prefix, wandb_model_ref)
 
     method = model_huggingface_url_to_preprocessing_style(model_context.huggingface_dataset)
 
     # Load dataset
     try:
-        ds = datasets.load_from_disk(str(Path(conf.save_datasets_path) / method.value / conf.split))
+        ds = datasets.load_from_disk(str(Path(save_datasets_path) / method.value / split))
 
         assert isinstance(ds, datasets.Dataset), f"Expexted a Dataset, but found: {type(ds)=}"
     except FileNotFoundError:
-        raise FileNotFoundError(f"\n\nYou should first run the dataset preprocessing script for {method.name=} and {conf.split=}")
+        raise FileNotFoundError(f"\n\nYou should first run the dataset preprocessing script for {method.name=} and {split=}")
 
-    features_cache_folder = Path(conf.features_cache_folder) / conf.wandb_prefix / conf.wandb_model_ref / method.value / conf.split
+    features_cache_folder = Path(features_cache_folder) / wandb_prefix / wandb_model_ref / method.value / split
 
     dataset = SingleDataset(OriginalRow(ds))
             
@@ -100,6 +91,20 @@ def main():
     os.makedirs(features_cache_folder, exist_ok=True)
 
     torch.save(features, features_cache_folder / "feats.pth")
+
+
+def main():
+
+    conf = OmegaConf.structured(Config)
+    conf.merge_with_cli()
+    conf = typing.cast(Config, conf)
+
+
+    for field in dataclasses.fields(Config):
+        getattr(conf, field.name)
+
+    save_features(conf.wandb_prefix, conf.wandb_model_ref, conf.split, conf.save_datasets_path, conf.features_cache_folder)
+
     
 
 if __name__ == "__main__":
